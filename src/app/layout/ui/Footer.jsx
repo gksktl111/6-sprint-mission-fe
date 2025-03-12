a824588b44029b572d2537797b757592e25ea8fc
gksktl111 <gksktl111@naver.com>
2025-03-12 20:22:30 +0900

    refactor : footer컴포넌트 BEM 규칙에 따른 className 수정 및 우측 이미지 렌더링 map으로 수정

--- src/app/layout/ui/Footer.jsx
+++ src/app/layout/ui/Footer.jsx
@@ -4,46 +4,48 @@ import twitter from '/img/ic_twitter.png';
 import youtube from '/img/ic_youtube.png';
 import instagram from '/img/ic_instagram.png';
 
+const LINK_IMG_DATAS = [
+  {
+    href: 'https://facebook.com',
+    img: facebook,
+  },
+  {
+    href: 'https://twitter.com',
+    img: twitter,
+  },
+  {
+    href: 'https://youtube.com',
+    img: youtube,
+  },
+  {
+    href: 'https://instagram.com',
+    img: instagram,
+  },
+];
+
 const Footer = () => {
+  console.log(LINK_IMG_DATAS);
   return (
-    <footer className={styles.footer__container}>
-      <div className={styles.footer__content}>
-        <div className={styles.footer__left}>
+    <footer className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.left}>
           <span>@codeit - 2025</span>
         </div>
-        <div className={styles.footer__center}>
+        <div className={styles.center}>
           <a href='/page/privacy.html'>Privacy Policy</a>
           <a href='/page/faq.html'>FAQ</a>
         </div>
-        <div className={styles.footer__right}>
-          <a
-            href='https://facebook.com'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <img src={facebook} alt='Facebook' />
-          </a>
-          <a
-            href='https://twitter.com'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <img src={twitter} alt='Twitter' />
-          </a>
-          <a
-            href='https://youtube.com'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <img src={youtube} alt='Youtube' />
-          </a>
-          <a
-            href='https://instagram.com'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <img src={instagram} alt='Instagram' />
-          </a>
+        <div className={styles.right}>
+          {LINK_IMG_DATAS.map((data) => (
+            <a
+              key={data.href}
+              href={data.href}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img src={data.img} alt={data.href} />
+            </a>
+          ))}
         </div>
       </div>
     </footer>
