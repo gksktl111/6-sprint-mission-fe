a87f7709863e5aab6817057e5c1005c8843df4fe
gksktl111 <gksktl111@naver.com>
2025-03-17 01:57:26 +0900

    fix : import 경로 오류 수정

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,7 +7,7 @@ import SignIn from '@pages/sign-in/SignIn';
 import Items from '@pages/items/Index';
 import Layout from './layout/Layout';
 import Registration from '@pages/registration/Index';
-import DetailItem from '@pages/detailItem/detailItem';
+import DetailItem from '@pages/detailItem/Index';
 
 function App() {
   return (
